480e71c00b59b116efaee5d0511ab13c07418f4e
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-04 14:41:36 -0600

    integrating React's componentDidMount() function to load robots + logging to see what was happening when

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            // robots: robots,
+            robots: [],
             searchfield: ''
         }
+        console.log('1 - End of constructor');
+    }
+
+    componentDidMount() { // part of React - no arrow functions
+        // console.log('check');
+        this.setState({ robots: robots });
+        console.log('2 - End of componentDidMount');
     }
 
     onSearchChange = (event) => {
@@ -26,6 +34,7 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        console.log('3 - prior to render return');
         return (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
