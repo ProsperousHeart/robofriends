2807a0c35d568566bc51b2f1b3e587579cfa3fda
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-04 00:04:40 -0600

    add interactivity with search box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import CardList from './CardList.js';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <CardList robots={robots}/>
-    )
+//const App = () => {
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event.target.value)
+    }
+
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
